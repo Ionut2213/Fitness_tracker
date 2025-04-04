d28519b31026aef72e82e050999176fcec6e4e48
laote <ihagiu1212@gmail.com>
2025-04-04 14:24:38 +0300

    in main.py i created a class called CSV this class manages the initilization of the csv file.if the file doesn t exists creates a new one with the columns from COLUMNS variable.File is created successfully

--- main.py
+++ main.py
@@ -2,3 +2,29 @@ import pandas as pd
 import csv
 from datetime import datetime
 import matplotlib as plt
+
+
+
+# Class that handle the CSV file
+
+class CSV:
+    # Variables 
+    CSV_FILE = 'fitness_tracker'
+    COLUMNS = ["date", "amount_of_km", "category", "note"]
+    FORMAT = "%d-%m-%Y"
+
+    @classmethod
+    def initializing_csv(cls):
+        try:
+            pd.read_csv(cls.CSV_FILE)
+        except FileNotFoundError:
+            df = pd.DataFrame(columns=cls.COLUMNS)
+            df.to_csv(cls.CSV_FILE, index=False)
+
+
+
+
+
+
+
+CSV.initializing_csv()
