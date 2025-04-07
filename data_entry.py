d619d0aad36cac9c0a26fd61c367dba23161995b
laote <ihagiu1212@gmail.com>
2025-04-07 12:24:08 +0300

    the data_entry.py file will handle the date input. get_date_from() asks for a date input from the user, checks is the date is in the correct format, plus allows you to get the current date if is not specified.

--- data_entry.py
+++ data_entry.py
@@ -1,2 +1,21 @@
 from datetime import datetime
 
+data_format = "%d-%m-%Y"
+
+CATEGORIES = {
+    "W": "Walk",
+    "IB": "Inside Bike"
+}
+
+
+def get_date_from(prompt, allow_default = True):
+    data_str = input(prompt)
+    if allow_default and not data_str:
+        return datetime.today().strftime(data_format)
+    
+    try:
+        valid_date = datetime.strptime(data_str, data_format)
+        return valid_date.strftime(data_format)
+    except ValueError:
+        print("You entered a wrong data format. Please enter the date in this format dd-mm-yyyy")
+        return get_date_from(prompt, allow_default)
