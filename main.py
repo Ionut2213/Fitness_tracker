109c1612f5b3f696baf6ea1d4a97b6ca3ac2ba06
laote <ihagiu1212@gmail.com>
2025-04-07 12:05:45 +0300

    method add_entry() handle the data added in csv file based on rows name.For now the app allows you to create csv files and add new entries in this csv file. add_entry is structured to not overwrite the new data entered, new data is added on a new line.Here i will need a new method to that will be used later on for extracting data

--- main.py
+++ main.py
@@ -21,6 +21,20 @@ class CSV:
             df = pd.DataFrame(columns=cls.COLUMNS)
             df.to_csv(cls.CSV_FILE, index=False)
 
+    @classmethod
+    def add_entry(cls, date, amount_of_km, category, note):
+        new_data_added = {
+            'date': date,
+            'amount_of_km': amount_of_km,
+            'category' : category,
+            'note' : note
+        }
+
+        with open(cls.CSV_FILE, "a", newline='') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=cls.COLUMNS)
+            writer.writerow(new_data_added)
+        print("Your new entry has been successfully added")
+
 
 
 
@@ -28,3 +42,4 @@ class CSV:
 
 
 CSV.initializing_csv()
+CSV.add_entry("22-03-205", 15, "walk", "outdoor walk")
